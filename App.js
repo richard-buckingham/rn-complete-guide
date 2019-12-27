df9ede22c8f6c66fbcfa8d13deb557d75cd7e9ac
richie.buckingham <richie.buckingham@gmail.com>
2019-12-27 22:31:05 +1100

    inline styles versus stylesheets

--- App.js
+++ App.js
@@ -3,23 +3,9 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center"
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal..."
-          style={{
-            width: "75%",
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal..." style={styles.input} />
         <Button title="ADD" />
       </View>
     </View>
@@ -27,10 +13,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: "#fff",
-  //     alignItems: "center",
-  //     justifyContent: "center"
-  //   }
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "75%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  }
 });
